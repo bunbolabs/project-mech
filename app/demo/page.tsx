b41be2fe676d90712ae3ff832c4ec36820f5a7d4
s0nhaaa <nguyensonha.hanz@gmail.com>
2024-03-23 21:12:32 +0700

    feat: preload

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,15 +1,21 @@
 'use client'
 
-import React from 'react'
 import { Unity, useUnityContext } from 'react-unity-webgl'
 
+import Preload from '@/components/preload'
+
 export default function Page() {
-  const { unityProvider } = useUnityContext({
+  const { unityProvider, isLoaded } = useUnityContext({
     loaderUrl: 'Build/demo-01.loader.js',
     dataUrl: 'Build/demo-01.data',
     frameworkUrl: 'Build/demo-01.framework.js',
     codeUrl: 'Build/demo-01.wasm',
   })
 
-  return <Unity className="h-screen w-screen" unityProvider={unityProvider} />
+  return (
+    <main className="h-screen w-screen overflow-hidden">
+      {!isLoaded && <Preload />}
+      <Unity className="h-screen w-screen" unityProvider={unityProvider} />
+    </main>
+  )
 }
---
+++ components/preload.tsx
@@ -0,0 +1,142 @@
+import { AnimatedText } from '@/components/animated-text'
+import Cursor from '@/components/cursor'
+import { NoiseBackground } from '@/components/noise-background'
+
+export default function Preload() {
+  return (
+    <section className="h-screen w-screen cursor-none overflow-hidden bg-black p-4">
+      <Cursor />
+      <div className=" flex h-full w-full items-center justify-center rounded-2xl border border-[#525252] font-medium">
+        <svg
+          className="absolute top-[8%]"
+          fill="none"
+          height="157"
+          viewBox="0 0 239 157"
+          width="239"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M96.4756 70.4L105.025 36.5493L108.722 39.3221L112.534 80.5667L96.4756 70.4Z" fill="#F2F2F2" />
+          <path
+            d="M71.9831 28L98.3242 58.1537L95.5515 69.5912L83.6518 59.1934L84.289 71.9019L85.6545 73.4038L79.9935 70.9776L65.7444 50.5286L71.9831 28Z"
+            fill="#F2F2F2"
+          />
+          <path
+            d="M96.9378 71.9019L117.618 85.6501L111.495 86.2277C96.6143 79.9428 96.6297 77.4088 96.9378 71.9019Z"
+            fill="#F2F2F2"
+          />
+          <path
+            d="M85.9623 63.0059L95.3203 72.2484L95.8979 77.6783L90.9301 75.5988L88.8505 74.559L86.6555 72.2484L85.9623 63.0059Z"
+            fill="#F2F2F2"
+          />
+          <path
+            d="M90.6992 81.491L92.7788 82.2997L96.2447 92.582L100.288 97.5498L109.993 120.541L99.7106 113.609L98.6709 107.832L87.1177 96.6256L90.6992 81.491Z"
+            fill="#F2F2F2"
+          />
+          <path
+            d="M104.332 103.095L112.188 106.908L113.343 113.84L112.419 116.843L110.109 114.071L109.415 114.764L104.332 103.095Z"
+            fill="#F2F2F2"
+          />
+          <path
+            d="M113.69 105.29L120.044 102.633V127.588L113.69 123.082L115.307 114.302L113.69 105.29Z"
+            fill="#F2F2F2"
+          />
+          <path
+            d="M114.73 50.6442L120.044 47.6404V84.0327L115.307 80.2202L112.534 54.4567L114.73 50.6442Z"
+            fill="#F2F2F2"
+          />
+          <g filter="url(#filter0_f_48_116)">
+            <path
+              d="M102.488 91.647L116.694 98.6843L110.876 102.402L101.811 99.6138L99.3761 95.232L94.5055 89.3897L90.1761 77.7051L63.5234 65.8877L54.9999 55.2654L64.3352 64.9583L93.0173 75.8462L97.6173 87.1325L102.488 91.647Z"
+              fill="white"
+            />
+          </g>
+          <path
+            d="M104.563 93.1595L116.694 99.2826L111.726 102.517L103.985 100.091L101.906 96.2788L97.7465 91.1954L94.0495 81.0287L71.2898 70.7464L64.0114 61.5039L71.983 69.9377L96.4756 79.4112L100.404 89.2314L104.563 93.1595Z"
+            fill="white"
+          />
+          <path d="M87.6954 88.4228L89.6594 80.6822L82.1499 77.2163L85.7314 82.4152L87.6954 88.4228Z" fill="#F2F2F2" />
+          <path
+            d="M111.957 88.4228L117.733 87.6141L116.809 96.9721L109.531 93.1596L102.368 88.4228L98.6707 81.0288C101.443 84.6334 108.683 87.46 111.957 88.4228Z"
+            fill="#F2F2F2"
+          />
+          <path d="M143.613 70.4L135.063 36.5493L131.366 39.3221L127.554 80.5667L143.613 70.4Z" fill="#F2F2F2" />
+          <path
+            d="M168.105 28L141.764 58.1537L144.537 69.5912L156.436 59.1934L155.799 71.9019L154.434 73.4038L160.095 70.9776L174.344 50.5286L168.105 28Z"
+            fill="#F2F2F2"
+          />
+          <path
+            d="M143.15 71.9019L122.47 85.6501L128.593 86.2277C143.474 79.9428 143.459 77.4088 143.15 71.9019Z"
+            fill="#F2F2F2"
+          />
+          <path
+            d="M154.126 63.0059L144.768 72.2484L144.19 77.6783L149.158 75.5988L151.238 74.559L153.433 72.2484L154.126 63.0059Z"
+            fill="#F2F2F2"
+          />
+          <path
+            d="M149.389 81.491L147.309 82.2997L143.843 92.582L139.8 97.5498L130.095 120.541L140.377 113.609L141.417 107.832L152.97 96.6256L149.389 81.491Z"
+            fill="#F2F2F2"
+          />
+          <path
+            d="M135.756 103.095L127.9 106.908L126.745 113.84L127.669 116.843L129.98 114.071L130.673 114.764L135.756 103.095Z"
+            fill="#F2F2F2"
+          />
+          <path
+            d="M126.398 105.29L120.044 102.633V127.588L126.398 123.082L124.781 114.302L126.398 105.29Z"
+            fill="#F2F2F2"
+          />
+          <path
+            d="M125.359 50.6442L120.044 47.6404V84.0327L124.781 80.2202L127.554 54.4567L125.359 50.6442Z"
+            fill="#F2F2F2"
+          />
+          <g filter="url(#filter1_f_48_116)">
+            <path
+              d="M137.6 91.647L123.395 98.6843L129.212 102.402L138.277 99.6138L140.712 95.232L145.583 89.3897L149.912 77.7051L176.565 65.8877L185.088 55.2654L175.753 64.9583L147.071 75.8462L142.471 87.1325L137.6 91.647Z"
+              fill="white"
+            />
+          </g>
+          <path
+            d="M135.525 93.1595L123.395 99.2826L128.362 102.517L136.103 100.091L138.183 96.2788L142.342 91.1954L146.039 81.0287L168.798 70.7464L176.077 61.5039L168.105 69.9377L143.612 79.4112L139.684 89.2314L135.525 93.1595Z"
+            fill="white"
+          />
+          <path d="M152.393 88.4228L150.429 80.6822L157.938 77.2163L154.357 82.4152L152.393 88.4228Z" fill="#F2F2F2" />
+          <path
+            d="M128.131 88.4228L122.355 87.6141L123.279 96.9721L130.557 93.1596L137.72 88.4228L141.417 81.0288C138.645 84.6334 131.405 87.46 128.131 88.4228Z"
+            fill="#F2F2F2"
+          />
+          <defs>
+            <filter
+              color-interpolation-filters="sRGB"
+              filterUnits="userSpaceOnUse"
+              height="156.137"
+              id="filter0_f_48_116"
+              width="170.694"
+              x="0.499878"
+              y="0.765381"
+            >
+              <feFlood flood-opacity="0" result="BackgroundImageFix" />
+              <feBlend in="SourceGraphic" in2="BackgroundImageFix" mode="normal" result="shape" />
+              <feGaussianBlur result="effect1_foregroundBlur_48_116" stdDeviation="27.25" />
+            </filter>
+            <filter
+              color-interpolation-filters="sRGB"
+              filterUnits="userSpaceOnUse"
+              height="154.537"
+              id="filter1_f_48_116"
+              width="169.094"
+              x="69.6945"
+              y="1.56538"
+            >
+              <feFlood flood-opacity="0" result="BackgroundImageFix" />
+              <feBlend in="SourceGraphic" in2="BackgroundImageFix" mode="normal" result="shape" />
+              <feGaussianBlur result="effect1_foregroundBlur_48_116" stdDeviation="26.85" />
+            </filter>
+          </defs>
+        </svg>
+
+        <AnimatedText className="mt-[5%]" text="LOADING ..."></AnimatedText>
+      </div>
+
+      <NoiseBackground />
+    </section>
+  )
+}
